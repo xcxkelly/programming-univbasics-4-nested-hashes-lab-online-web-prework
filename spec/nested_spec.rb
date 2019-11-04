d953ace2ce9ccdb7847cff010805d6ff51b68556
Kelly Small <xcxkelly98@gmail.com>
2019-11-04 15:49:49 +0000

    Done.

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -39,3 +39,9 @@ describe "nested hash" do
     end
   end
 end
+
+my_hash = {first: "I'm first!", second: "I'm second!"}
+my_hash[:third] = "I'm third!"
+puts my_hash = {first: "I'm first!", second: "I'm second!"}
+my_hash =  {first: "I'm first!", second: "I'm second!", third: "I'm third!"}
+end

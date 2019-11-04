b20206d43d80860b723c767475cd5f6ff9d60fff
Kelly Small <xcxkelly98@gmail.com>
2019-11-04 15:49:37 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -43,3 +43,5 @@ end
 my_hash = {first: "I'm first!", second: "I'm second!"}
 my_hash[:third] = "I'm third!"
 puts my_hash = {first: "I'm first!", second: "I'm second!"}
+my_hash =  {first: "I'm first!", second: "I'm second!", third: "I'm third!"}
+end
